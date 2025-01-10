29ad2d5cbb6fc0d79f2a2cbb396505244f6ae563
Franco Caraffa <francocaraffa2005@gmail.com>
2025-01-10 16:17:12 -0300

    Bugs arreglados

--- Mazo.cpp
+++ Mazo.cpp
@@ -9,21 +9,18 @@ Mazo::Mazo(): m_mazo{TODAS} {
 }
 
 vector<Carta>Mazo::Obtener3cartas(){
-	int carta1 = -1;
-	int carta2 = -1;
-	int carta3 = -1;
 	
-	while(carta1 == carta2 or carta1 == carta3 or carta2 == carta3){
-		carta1 = rand()%m_mazo.size();  
-		carta2 = rand()%m_mazo.size(); 	
-		carta3 = rand()%m_mazo.size();	
+	int cont = 0;
+	vector<Carta> cartas3;
+	while(cont < 3){
+		cont ++;
+		int carta = rand()%m_mazo.size();  
+		cartas3.push_back(m_mazo[carta]);
+		m_mazo.erase(m_mazo.begin() + carta);
 	}
 	
-	vector<Carta> cartas3{m_mazo[carta1], m_mazo[carta2], m_mazo[carta3]};
-	m_mazo.erase(m_mazo.begin() + carta1);
-	m_mazo.erase(m_mazo.begin() + carta2);
-	m_mazo.erase(m_mazo.begin() + carta3);
 	return cartas3;
+	
 }
 
 void Mazo::reiniciar_mazo(){
--- ValoresCartas.h
+++ ValoresCartas.h
@@ -47,22 +47,26 @@
 #define CARTA25 "./images/oro10.png", 10, oro, 10   
 #define CARTA26 "./images/copa10.png", 10, copa, 10
 //
-#define CARTA27 "./images/espada6.png", 11, espada, 6
-#define CARTA28 "./images/basto6.png", 11, basto, 6
-#define CARTA29 "./images/oro6.png", 11, oro, 6   
-#define CARTA30 "./images/copa6.png", 11, copa, 6
-//
-#define CARTA31 "./images/espada5.png", 12, espada, 5
-#define CARTA32 "./images/basto5.png", 12, basto, 5
-#define CARTA33 "./images/oro5.png", 12, oro, 5   
-#define CARTA34 "./images/copa5.png", 12, copa, 5
-//
-#define CARTA35 "./images/espada4.png", 13, espada, 4
-#define CARTA36 "./images/basto4.png", 13, basto, 4
-#define CARTA37 "./images/oro4.png", 13, oro, 4   
-#define CARTA38 "./images/copa4.png", 13, copa, 4
 
-#define TODAS Carta(CARTA1), Carta(CARTA2), Carta(CARTA3), Carta(CARTA4), Carta(CARTA5), Carta(CARTA6), Carta(CARTA7), Carta(CARTA8), Carta(CARTA9), Carta(CARTA10), Carta(CARTA11), Carta(CARTA12), Carta(CARTA13), Carta(CARTA14), Carta(CARTA15), Carta(CARTA16), Carta(CARTA17), Carta(CARTA18), Carta(CARTA19), Carta(CARTA20), Carta(CARTA21), Carta(CARTA22), Carta(CARTA23), Carta(CARTA24), Carta(CARTA25), Carta(CARTA26), Carta(CARTA27), Carta(CARTA28), Carta(CARTA29), Carta(CARTA30), Carta(CARTA31), Carta(CARTA32), Carta(CARTA33), Carta(CARTA34), Carta(CARTA35), Carta(CARTA36), Carta(CARTA37), Carta(CARTA38) 
+#define CARTA27 "./images/basto7.png", 11, basto, 7
+#define CARTA28 "./images/copa7.png", 11, copa, 7
+
+#define CARTA29 "./images/espada6.png", 12, espada, 6
+#define CARTA30 "./images/basto6.png", 12, basto, 6
+#define CARTA31 "./images/oro6.png", 12, oro, 6   
+#define CARTA32 "./images/copa6.png", 12, copa, 6
+//
+#define CARTA33 "./images/espada5.png", 13, espada, 5
+#define CARTA34 "./images/basto5.png", 13, basto, 5
+#define CARTA35 "./images/oro5.png", 13, oro, 5   
+#define CARTA36 "./images/copa5.png", 13, copa, 5
+//
+#define CARTA37 "./images/espada4.png", 14, espada, 4
+#define CARTA38 "./images/basto4.png", 14, basto, 4
+#define CARTA39 "./images/oro4.png", 14, oro, 4   
+#define CARTA40 "./images/copa4.png", 14, copa, 4
+
+#define TODAS Carta(CARTA1), Carta(CARTA2), Carta(CARTA3), Carta(CARTA4), Carta(CARTA5), Carta(CARTA6), Carta(CARTA7), Carta(CARTA8), Carta(CARTA9), Carta(CARTA10), Carta(CARTA11), Carta(CARTA12), Carta(CARTA13), Carta(CARTA14), Carta(CARTA15), Carta(CARTA16), Carta(CARTA17), Carta(CARTA18), Carta(CARTA19), Carta(CARTA20), Carta(CARTA21), Carta(CARTA22), Carta(CARTA23), Carta(CARTA24), Carta(CARTA25), Carta(CARTA26), Carta(CARTA27), Carta(CARTA28), Carta(CARTA29), Carta(CARTA30), Carta(CARTA31), Carta(CARTA32), Carta(CARTA33), Carta(CARTA34), Carta(CARTA35), Carta(CARTA36), Carta(CARTA37), Carta(CARTA38), Carta(CARTA39), Carta(CARTA40) 
 	
 
 #endif
