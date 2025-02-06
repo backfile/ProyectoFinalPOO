6847e63818b6f465960f2bd47d318caa21ec9d7c
Franco Caraffa <francocaraffa09@gmail.com>
2025-02-05 23:25:53 -0300

    Detalles al pasar el mouse por los botones del menu y deteccion de clicks

--- Menu.cpp
+++ Menu.cpp
@@ -6,14 +6,47 @@
 Menu::Menu(Window *w) : m_window(w) {
 	t_fondo.loadFromFile("images/fondomenu.png");
 	s_fondo.setTexture(t_fondo);
+	
+	t_NuevaPartidaBoton_porEncima.loadFromFile("images/nuevapartida_porencima.png");
+	s_NuevaPartidaBoton_porEncima.setTexture(t_NuevaPartidaBoton_porEncima);
+	NuevaPartidaBoton.setSize(Vector2f(196, 152));
+	NuevaPartidaBoton.setPosition(301, 85);
+	
+	t_HistorialBoton_porEncima.loadFromFile("images/historial_porencima.png");
+	s_HistorialBoton_porEncima.setTexture(t_HistorialBoton_porEncima);
+	HistorialBoton.setSize(Vector2f(230, 120));
+	HistorialBoton.setPosition(292, 388);
+	
 }
 
 void Menu::actualizar(Juego &j){
-	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
-		j.actualizarEscena(new Match(m_window));
+	mousePos = sf::Mouse::getPosition(*m_window);
+	if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+		mousePos = sf::Mouse::getPosition(*m_window);
+		if(NuevaPartidaBoton.getGlobalBounds().contains(static_cast<sf::Vector2f>(mousePos))){
+			j.actualizarEscena(new Match(m_window));
+		}
+	}
+	if(NuevaPartidaBoton.getGlobalBounds().contains(static_cast<sf::Vector2f>(mousePos))){
+		NuevaPartidaBoton_porEncima = true;
+	}else{
+		NuevaPartidaBoton_porEncima = false;
+	}
+	
+	
+	if(HistorialBoton.getGlobalBounds().contains(static_cast<sf::Vector2f>(mousePos))){
+		HistorialBoton_porEncima = true;
+	}else{
+		HistorialBoton_porEncima = false;
 	}
 }
 
 void Menu::dibujar(RenderWindow &w){
 	w.draw(s_fondo);
+	if(NuevaPartidaBoton_porEncima){
+		w.draw(s_NuevaPartidaBoton_porEncima);
+	}
+	if(HistorialBoton_porEncima){
+		w.draw(s_HistorialBoton_porEncima);
+	}
 }
--- Menu.h
+++ Menu.h
@@ -14,9 +14,25 @@ public:
 	void actualizar(Juego &j) override; //Se ejecuta siempre
 	void dibujar(RenderWindow &w) override;   //Se ejecuta siempre
 private:
+	bool NuevaPartidaBoton_porEncima = false;
+	bool HistorialBoton_porEncima = false;
+	
+	
 	Window *m_window;
 	Texture t_fondo;
 	Sprite s_fondo; 
+	
+	Texture t_NuevaPartidaBoton_porEncima;
+	Sprite s_NuevaPartidaBoton_porEncima;
+	
+	Texture t_HistorialBoton_porEncima;
+	Sprite s_HistorialBoton_porEncima;
+	
+	sf::RectangleShape NuevaPartidaBoton;
+	sf::RectangleShape HistorialBoton;
+	
+	
+	sf::Vector2i mousePos;
 };
 
 #endif
